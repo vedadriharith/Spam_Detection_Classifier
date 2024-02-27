fd2f477afd85dd432a33e83e38c02534dfc7e702
vedadriharith <vedadriharith@gmail.com>
2024-02-27 23:55:38 +0000

    added api route

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ def predict_api():
     tokenized_email=cv.transform([email])
     prediction=clf.predict(tokenized_email)
     prediction=1 if prediction==1 else -1
-    return jsonify({prediction:prediction})
+    return jsonify({'prediction':prediction, 'email':email})
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
