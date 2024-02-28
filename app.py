500c1fab114871465a37827896d30b14eed01bb2
vedadriharith <vedadriharith@gmail.com>
2024-02-28 00:16:15 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from utils.py import make_prediction
+from utils import make_prediction
 app=Flask(__name__)
 @app.route("/")
 def home():
