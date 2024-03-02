02e98f91c358bf9881bdef3b4f4747b551aea730
vedadriharith <vedadriharith@gmail.com>
2024-03-02 06:45:00 +0000

    changes

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def predict_api():
     prediction=make_prediction(email)
     return jsonify({'prediction':prediction, 'email':email})
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
